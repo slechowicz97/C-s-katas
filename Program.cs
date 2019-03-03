15f2395ba233fdf65636758413bd1be9ce45cfa5
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-03 13:47:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfPossitive
+{
+    class Program
+    {
+        static int PositiveSum(double[] arr)
+        {
+            int sum = 0;
+            foreach (int element in arr)
+            {
+                if (element > 0)
+                {
+                    sum += element;
+                }
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            double[] a = new double[10];
+            for (int i=0; i<10; i++)
+            {
+                a[i] = rand.Next(-160, 160);
+                Console.WriteLine(a[i]);
+            }
+            Console.WriteLine(PositiveSum(a));
+            Console.ReadKey();
+        }
+
+    }
+}
+
