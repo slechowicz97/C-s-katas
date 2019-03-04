5c8423648836eb887c1f5b26374c7bb2687a25e5
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-04 22:23:13 +0100

    Add files via upload
    Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
    The output should be two capital letters with a dot seperating them.
    It should look like this:
    
    Sam Harris => S.H
    Patrick Feeney => P.F

---
+++ Abbreviate_a_Two_Word_Name.cs
@@ -0,0 +1,32 @@
+﻿
+/* Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+The output should be two capital letters with a dot seperating them.
+It should look like this:
+Sam Harris => S.H
+Patrick Feeney => P.F
+*/
+
+using System;
+namespace ConsoleApp1
+{
+   class Program
+   {
+       public static string AbbrevName(string name)
+       {
+           string c = "", a = "";
+           for (int i = 0; i < name.Length; i++)
+           {
+               if (name[i] == 32) { i++; c = System.Convert.ToString(name[i]); a = System.Convert.ToString(name[0]); break; }
+           }
+           return a.ToUpper() + "." + c.ToUpper();
+       }
+
+       static void Main(string[] args)
+       {
+           string first = "Alicja Kowalska", second = "Jan Nowak";
+           Console.WriteLine(AbbrevName(first));
+           Console.WriteLine(AbbrevName(second));
+           Console.ReadKey();
+       }
+   }
+}
