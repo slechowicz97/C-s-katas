e21d7221e93746c62785537b7fdc02b7a49f512b
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-07 19:33:43 +0100

    Add files via upload
    Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
    
    Example:
    isDivisible(3,1,3)--> true because 3 is divisible by 1 and 3
    isDivisible(12,2,6)--> true because 12 is divisible by 2 and 6
    isDivisible(100,5,3)--> false because 100 is not divisible by 3
    isDivisible(12,7,5)--> false because 12 is neither divisible by 7 nor 5

---
+++ is_n_divisible_by_x_and_y.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Trzeci
+{
+    class Program
+    {
+            public static bool isDivisible(long n, long x, long y)
+            {
+                bool a = true;
+                if ((n % x == 0) && (n % y == 0)) { return a; }
+                else return false;
+            }
+        
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            Console.WriteLine("Podaj liczbę dla której sprawdzasz podzielność");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj potencjalny dzielnik");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj drugi potencjalny dzielnik");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isDivisible(a,b,c));
+            Console.ReadKey();
+        }
+    }
+}
