68e4d2fae7720a815e1e52b99e33251aa7683f01
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-07 20:13:31 +0100

    Add files via upload
    Task Overview:
    You have to write a function that accepts three parameters:
    
    cap is the amount of people the bus can hold excluding the driver.
    on is the number of people on the bus.
    wait is the number of people waiting to get on to the bus.
    If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
    
    Usage Examples:
    Kata.Enough(10, 5, 5);
    // 0, He can fit all 5 passengers
    Kata.Enough(100, 60, 50);
    // 10, He can't fit 10 out of 50 waiting

---
+++ Passenger_waiting_for_a_bus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passengers_waiting_for_a_bus
+{
+    class Program
+    {
+        public static int Enough(int cap, int on, int wait)
+        {
+            return Math.Abs(cap - on - wait);
+
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            int cap, on = 0, wait = 0;
+            Console.WriteLine("Podaj ilosc miejsc w autobusie");
+            cap = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj ilosc ludzi znajdujacych sie w autobusie");
+            on = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj ilosc ludzi na przystanku");
+            wait = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Autobus nie zabierze {0} pasazerow",Enough(cap, on, wait));
+            Console.ReadKey();
+        }
+
+    }
+}
