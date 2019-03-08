f4d69842487f4c9f00b2b137867b5d30e3d19e86
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-08 22:14:15 +0100

    Find the first non-consecutive number
    Your task is to find the first element of an array that is not consecutive.
    
    E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non consecutive number.
    
    If the whole array is consecutive then return null
    
    The array will always have at least 2 elements and all the elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!

---
+++ Find the first non-consecutive number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Solution
+{
+    public static int Solution1(int[] A)
+    {   
+        Array.Sort(A);
+        for (int i = 0; i < A.Length; i++)
+        {
+            Console.WriteLine(A[i]);
+        }
+        int c = 0;
+        for (int i = 0; i < A.Length - 1; i++)
+        {
+            if (A[i] == A[i+1]) { continue; }
+            if (A[i] + 1 != A[i + 1])
+            {
+                return A[i]+1;
+            }
+        }
+        return 0;
+    }
+
+static void Main(string[] args)
+        {
+        int[] A = { 11, 31, 6, 3, 1, 2 };
+        Console.WriteLine(Solution1(A));
+
+        Console.WriteLine(A[0]);
+        Console.ReadKey();
+        }
+    
+}
+
