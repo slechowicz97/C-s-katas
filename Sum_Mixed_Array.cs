766e8516917f705432e024315e10aa7c8b326097
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-13 19:56:56 +0100

    Sum Mixed Array
    Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
    
    Return your answer as a number.

---
+++ Sum_Mixed_Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum_Mixed_Array
+{
+    class Program
+    {
+        public static int SumMix(object[] x)
+        {
+            int a = 0, sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                a = System.Convert.ToInt32(x[i]);
+                sum = sum + a;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            object[] tab = { "5", "0", 9, 3, 2, 1, "9", 6, 7 };
+            Console.WriteLine(SumMix(tab));
+            Console.ReadKey();
+        }
+    }
+}
