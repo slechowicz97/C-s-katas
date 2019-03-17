fbf3db9776d29c8ac19f8d82217128904e295728
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-17 15:21:59 +0100

    Rental Car Cost
    After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
    
    You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
    
    Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
    
    Write a code that gives out the total amount for different days(d).

---
+++ RentalCarCost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Rent_Cars
+{
+    class Program
+    {
+
+     public static int RentalCarCost(int d)
+            {
+                if (d >= 7) { return d * 40 - 50; }
+                else if (d >= 3) { return d * 40 - 20; }
+                else return d * 40;
+            }
+        
+        static void Main(string[] args)
+        {
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(RentalCarCost(d));
+            Console.ReadKey();
+        }
+    }
+}
