c81849c4ebfc7712edc5755863032822615ab709
slechowicz97 <33069878+slechowicz97@users.noreply.github.com>
2019-03-17 16:46:49 +0100

    String ends with
    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
    
    
    Examples:
    
    solution('abc', 'bc') // returns true
    solution('abc', 'd') // returns false

---
+++ String_ends_with.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace String_ends_with
+{
+    class Program
+    {
+            public static bool Solution(string str, string ending)
+            {
+                int b = 0;
+                for (int i = str.Length - ending.Length; i < str.Length; i++)
+                {
+                    if (i < 0) { return false; }
+                    if (str[i] == ending[b]) { b++; }
+                    else { return false; }
+                }
+                return true;
+            }
+        static void Main(string[] args)
+        {
+            string str;
+            string end;
+
+            str = Convert.ToString(Console.ReadLine());
+            end = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(Solution(str, end));
+
+            Console.ReadKey();
+        }
+    }
+}
